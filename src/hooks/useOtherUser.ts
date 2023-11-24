a0e3d9dd5ba42fca7b4e8ea742f31b03a38131f1
Agustin Petrillo <agustinpetrillo1@gmail.com>
2023-11-24 18:07:56 -0300

    otherUser hook

---
+++ src/hooks/useOtherUser.ts
@@ -0,0 +1,23 @@
+import { useMemo } from "react";
+import { useSession } from "next-auth/react";
+import { User } from "@prisma/client";
+
+const useOtherUser = (
+  conversation: FullConversationType | { users: User[] }
+) => {
+  const session = useSession();
+
+  const otherUser = useMemo(() => {
+    const currentUserEmail = session?.data?.user?.email;
+
+    const otherUsers = conversation.users.filter(
+      (user) => user.email !== currentUserEmail
+    );
+
+    return otherUsers;
+  }, [session?.data?.user?.email, conversation.users]);
+
+  return otherUser;
+};
+
+export default useOtherUser;
